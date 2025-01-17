72d185599111369ffb088f2e0b113d1e56f7a8e6
Varpenikhil0007 <varpenikhil0007@gmail.com>
2025-01-17 15:33:24 +0530

    first commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,40 +1,15 @@
-// // // Import the functions you need from the SDKs you need
-// // import { initializeApp } from "firebase/app";
-// // import { getFirestore } from "firebase/firestore";
-
-// // // Your web app's Firebase configuration
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyBnJj_K1WzecQijn7sqDf4OcbEl_Etpb2Q",
-// //   authDomain: "products-n-cart.firebaseapp.com",
-// //   projectId: "products-n-cart",
-// //   storageBucket: "products-n-cart.appspot.com",
-// //   messagingSenderId: "1004531008608",
-// //   appId: "1:1004531008608:web:e39dd7636b7bedbb04af05",
-// // };
-
-// // // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
-
-// // // Initialize Cloud Firestore and get a reference to the service
-// // export const db = getFirestore(app);
-
-
-
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
 // const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
+//   apiKey: "AIzaSyBnJj_K1WzecQijn7sqDf4OcbEl_Etpb2Q",
+//   authDomain: "products-n-cart.firebaseapp.com",
+//   projectId: "products-n-cart",
+//   storageBucket: "products-n-cart.appspot.com",
+//   messagingSenderId: "1004531008608",
+//   appId: "1:1004531008608:web:e39dd7636b7bedbb04af05",
 // };
 
 // // Initialize Firebase
@@ -42,3 +17,28 @@
 
 // // Initialize Cloud Firestore and get a reference to the service
 // export const db = getFirestore(app);
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
