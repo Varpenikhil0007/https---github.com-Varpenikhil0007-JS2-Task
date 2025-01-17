ecda2b8148a6eed7d0fe11f73bcbadcf5e673d88
Varpenikhil0007 <varpenikhil0007@gmail.com>
2025-01-17 11:27:56 +0530

    first

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -18,38 +18,21 @@ export const check_for_bread = (cartItems) => {
   return false;
 };
 
-// returns the savings on an item if offer applicable
 export const check_for_offers = (item, itemPrice, isSoupPresent) => {
-  // buy 1 get 1 free on cheese!
-  // buy a soup and get bread for half the price!
-  // get 1/3rd off on butter!
   let saving = 0;
   switch (item.id) {
-    case 1:
-      // Bread
+    case 1: // Bread
       if (isSoupPresent) saving = itemPrice / 2;
       break;
-    // case 5:
-    //   // Butter
-    //   saving = itemPrice / 3;
-    //   break;
+    case 3: // Cheese (Buy 1 Get 1 Free)
+      saving = itemPrice; // Saving the cost of one cheese as free
+      break;
+    case 5: // Butter
+      saving = itemPrice / 3; // 33% off
+      break;
     default:
       saving = 0;
       break;
-  }
-  const offer_id = item.offer_id;
-  if (offer_id) {
-    switch (offer_id) {
-      case "1":
-        saving = itemPrice / 3;
-        break;
-      case "2":
-        saving = itemPrice / 2;
-        break;
-      default:
-        saving = 0;
-        break;
-    }
   }
   return saving;
 };
