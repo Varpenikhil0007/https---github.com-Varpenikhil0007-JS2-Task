901f9975ea2f0172ce9350c4473e0502ab996f87
Varpenikhil0007 <varpenikhil0007@gmail.com>
2025-01-17 10:06:23 +0530

    first

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -28,13 +28,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDo5QFgU46-i1K2C1YBH2bBlHqvSJ8qSuc",
-  authDomain: "product-asse.firebaseapp.com",
-  databaseURL: "https://product-asse-default-rtdb.firebaseio.com",
-  projectId: "product-asse",
-  storageBucket: "product-asse.firebasestorage.app",
-  messagingSenderId: "608447195598",
-  appId: "1:608447195598:web:d5835ee5af12b2b838e39f"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
